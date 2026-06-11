de55b3d676c8bd1144abae924956407b5c40b2e7
mohammaddraz <22210035@htu.edu.jo>
2026-06-11 23:33:31 +0300

    feat: added PostgreSQL schema, seed data, and JWT authentication logic

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Health Check Route
 app.get('/api/health', (req, res) => {
